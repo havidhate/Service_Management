6993820eda9e30f3af31ec0abbde91d3fb144c4e
Hutesh <huteshvidhate23@gmail.com>
2021-06-29 12:26:04 +0530

    created

---
+++ src/test/java/com/example/serveice/ServeiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serveice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
