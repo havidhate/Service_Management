d6617abd1977673c58102a0262f2e6533e224f27
Hutesh <huteshvidhate23@gmail.com>
2021-07-06 16:20:39 +0530

    hutesh commit

---
+++ src/main/java/Country_Master/ThirdTestApplication.java
@@ -0,0 +1,14 @@
+package state_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdTestApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/state_master/Dao/Dao.java
@@ -0,0 +1,9 @@
+package state_master.ThirdTest.Dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import state_master.ThirdTest.enitity.Model;
+
+public interface Dao extends JpaRepository<Model, Long>{
+
+}
