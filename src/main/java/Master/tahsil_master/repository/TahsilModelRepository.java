ee8af4bafae554f7c7beae837b57672549c50ee4
AdityaKumar157 <49588338+AdityaKumar157@users.noreply.github.com>
2021-07-13 22:50:15 +0530

    Create TahsilModelRepository.java

---
+++ src/main/java/Master/tahsil_master/repository/TahsilModelRepository.java
@@ -0,0 +1,7 @@
+package Master.tahsil_master.repository;
+
+import Master.tahsil_master.entity.TahsilModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TahsilModelRepository  extends JpaRepository<TahsilModel, Integer> {
+}
