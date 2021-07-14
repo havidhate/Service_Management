bf085a33adb0afdd2110d7ead8924b55991f91f6
AdityaKumar157 <49588338+AdityaKumar157@users.noreply.github.com>
2021-07-14 16:41:17 +0530

    Create DistrictModelRepository.java

---
+++ src/main/java/Master/district_master/repository/DistrictModelRepository.java
@@ -0,0 +1,7 @@
+package Master.district_master.repository;
+
+import Master.district_master.entity.DistrictModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DistrictModelRepository  extends JpaRepository<DistrictModel, Integer> {
+}
