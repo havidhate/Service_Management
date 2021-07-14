7bc89bad2465cab5de4b418d92dd6b1d5f9be231
AdityaKumar157 <49588338+AdityaKumar157@users.noreply.github.com>
2021-07-14 16:39:17 +0530

    Delete src/main/java/Master/district_master directory

--- src/main/java/Master/district_master/DistrictDao/DistrictDao.java
+++
@@ -1,7 +0,0 @@
-package Master.district_master.DistrictDao;
-
-import Master.district_master.DistrictEntity.DistrictModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DistrictDao extends JpaRepository<DistrictModel, Long> {
-}
